f5e0bbeb18034ff48ac3f176e1159f6d6847a7b0
drift2galaxy <drift2galaxy@gmail.com>
2022-06-22 13:47:35 +0800

    removed deploying to vm ubuntu

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app.include_router(vote.router)
 # for reference
 @app.get("/")
 def root():
-    return {"message": "Hello World! pushing out to ubuntu"}
+    return {"message": "FastApi Project finished"}
